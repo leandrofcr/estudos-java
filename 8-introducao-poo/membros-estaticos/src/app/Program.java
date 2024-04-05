00afc959363be57793a8da7100246461f8b0c6ef
Leandro <lleandrofr@gmail.com>
2024-04-05 17:10:12 -0300

    Add content about static members

---
+++ 8-introducao-poo/membros-estaticos/src/app/Program.java
@@ -0,0 +1,29 @@
+package app;
+
+import util.Calculator;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter radius: ");
+
+        double radius = sc.nextDouble();
+        /*
+         *Os método circumference e volume são métodos estáticos para serem utilizados não precisam ser instanciados
+         * Nesse caso basta acessa-los utilizando a classe utilizatária Calculator
+         */
+
+        double c = Calculator.circumference(radius);
+        double v = Calculator.volume(radius);
+
+        System.out.printf("Circumference: %.2f%n", c);
+        System.out.printf("Volume: %.2f%n", v);
+        System.out.printf("PI: %.2f%n", Calculator.PI);
+
+    }
+}
