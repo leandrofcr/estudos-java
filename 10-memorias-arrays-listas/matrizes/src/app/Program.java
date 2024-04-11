c5f22186afa685f147aa97499155918016f75ac9
Leandro <lleandrofr@gmail.com>
2024-04-11 14:35:16 -0300

    feat: add notes about java multidimensional arrays (matrizes)

---
+++ 10-memorias-arrays-listas/matrizes/src/app/Program.java
@@ -0,0 +1,43 @@
+package app;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[][] mat = new int[n][n];
+
+        // quantidade de linhas -> mat.length
+        // quantidade de colunas -> mat[i].length
+
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; mat[i].length < n; j++) {
+                mat[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Main diagonal: ");
+        for (int i = 0; i < mat.length; i++) {
+            System.out.print(mat[i][i] + " ");
+        }
+
+        System.out.println();
+
+        int counter = 0;
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                if (mat[i][j] < 0) {
+                    counter++;
+                }
+            }
+        }
+
+        System.out.println("Negative numbers: " + counter);
+
+        sc.close();
+    }
+}
