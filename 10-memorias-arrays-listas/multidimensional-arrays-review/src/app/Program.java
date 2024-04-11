6479ab27d223fe01ad878af0b6aed956b0eb6b3b
Leandro <lleandrofr@gmail.com>
2024-04-11 14:42:03 -0300

    fix for conditions

---
+++ 10-memorias-arrays-listas/multidimensional-arrays-review/src/app/Program.java
@@ -0,0 +1,29 @@
+package app;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows and columns: ");
+        int rows = sc.nextInt();
+        int columns = sc.nextInt();
+        System.out.println(rows);
+        System.out.println(columns);
+
+        int[][] mat = new int[rows][columns];
+
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; mat[i].length < n; j++) {
+                mat[i][j] = sc.nextInt();
+            }
+        }
+
+
+        sc.close();
+
+
+    }
+}
