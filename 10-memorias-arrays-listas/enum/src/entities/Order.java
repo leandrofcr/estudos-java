382774e0bbfffbd3241e133e45608b8eb1cb4877
Leandro <lleandrofr@gmail.com>
2024-04-12 12:13:00 -0300

    feat: add notes about Enum syntax

---
+++ 10-memorias-arrays-listas/enum/src/entities/Order.java
@@ -0,0 +1,54 @@
+package entities;
+
+import entities.enums.OrderStatus;
+
+import java.time.Instant;
+import java.util.Date;
+
+public class Order {
+    private Integer id;
+    private Instant moment;
+    private OrderStatus status;
+
+    public Order() {
+    }
+
+    public Order(Integer id, Instant moment, OrderStatus status) {
+        this.id = id;
+        this.moment = moment;
+        this.status = status;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", moment=" + moment +
+                ", status=" + status +
+                '}';
+    }
+}
