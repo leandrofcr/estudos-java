f3d63ec1157b6336fd65a0fd16137cd67457e95f
Leandro <lleandrofr@gmail.com>
2024-04-15 16:20:08 -0300

    feat: add review exercise about composition

---
+++ 13-enum-composicao/review-exercise-composition/src/application/Program.java
@@ -0,0 +1,59 @@
+package application;
+
+import entities.enums.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter customer data:");
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+        System.out.print("Email: ");
+        String email = sc.nextLine();
+        System.out.print("Birth date (DD/MM/YYYY): ");
+        Date birthDate = sdf.parse(sc.next());
+        System.out.println("Enter order data:");
+
+        System.out.print("Status: ");
+        String status = sc.next();
+        OrderStatus orderStatus = OrderStatus.valueOf(status);
+        System.out.print("How many items to this order: ");
+        int numberOfItems = sc.nextInt();
+        sc.nextLine();
+
+        Order order = new Order(new Date(), orderStatus, new Customer(name, email, birthDate));
+
+
+        for (int i = 1; i <= numberOfItems; i++) {
+            System.out.printf("Enter #%s%n item data: ", i);
+            System.out.print("Product name: ");
+            String productName = sc.nextLine();
+            System.out.print("Product price: ");
+            double productPrice = sc.nextDouble();
+            System.out.print("Quantity: ");
+            int quantity = sc.nextInt();
+            sc.nextLine();
+            double orderPrice = productPrice * quantity;
+
+            OrderItem item = new OrderItem(quantity, orderPrice, new Product(productName, productPrice));
+
+            order.addItem(item);
+        }
+
+        System.out.println();
+        System.out.println("ORDER SUMMARY: ");
+        System.out.println(order);
+
+
+        sc.close();
+    }
+}
---
+++ 13-enum-composicao/review-exercise-composition/src/entities/enums/Order.java
@@ -0,0 +1,81 @@
+package entities.enums;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Order {
+    public static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    public static SimpleDateFormat sdfWithHour = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+    private final Date moment;
+    private OrderStatus status;
+    private final Customer customer;
+
+    List<OrderItem> items = new ArrayList<>();
+
+    public Order(Date moment, OrderStatus status, Customer customer) {
+        this.moment = moment;
+        this.status = status;
+        this.customer = customer;
+    }
+
+    public Date getMoment() {
+        return moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public void addItem(OrderItem orderItem) {
+        items.add(orderItem);
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void removeItem(OrderItem orderItem) {
+        items.remove(orderItem);
+    }
+
+    public double total() {
+        double total = 0;
+
+        for (OrderItem item : items) {
+            total += item.subTotal();
+        }
+        return total;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Order moment: ").append(sdfWithHour.format(moment)).append("\n");
+        sb.append("Order status: ").append(status).append("\n");
+        sb.append("Customer: ").append(customer.getName()).append(" ");
+        sb.append("(").append(sdf.format(customer.getBirthDate())).append(") - ").append(customer.getEmail()).append("\n");
+        sb.append("Order items:").append("\n");
+
+        for (OrderItem item : items) {
+            sb.append(item.getProduct().getName()).append(", $");
+            sb.append(formatCurrency(item.getProduct().getPrice())).append(", ");
+            sb.append("Quantity: ").append(item.getQuantity()).append(", $");
+            sb.append(formatCurrency(item.subTotal())).append("\n");
+        }
+        sb.append("Total price: $").append(formatCurrency(total()));
+
+        return sb.toString();
+    }
+
+    public String formatCurrency(double value) {
+        return String.format("%.2f", value);
+    }
+
+}
