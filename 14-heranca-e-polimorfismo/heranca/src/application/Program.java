297787b20de26f753e250f8d70617b9bd7142d94
Leandro <lleandrofr@gmail.com>
2024-04-15 22:51:07 -0300

    feat: create a subclass to exemplify the concept of inheritance

---
+++ 14-heranca-e-polimorfismo/heranca/src/application/Program.java
@@ -0,0 +1,9 @@
+package application;
+
+import entities.BusinessAccount;
+
+public class Program {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 14-heranca-e-polimorfismo/heranca/src/entities/BusinessAccount.java
@@ -0,0 +1,28 @@
+package entities;
+
+public class BusinessAccount extends Account {
+    private Double loanLimit;
+
+    public BusinessAccount() {
+        super();
+    }
+
+    public BusinessAccount(Integer number, String holder, Double balance, Double loanLimit) {
+        super(number, holder, balance); //executa a lógica do constructor da class base ou super class (Account)
+        this.loanLimit = loanLimit;
+    }
+
+    public Double getLoanLimit() {
+        return loanLimit;
+    }
+
+    public void setLoanLimit(Double loanLimit) {
+        this.loanLimit = loanLimit;
+    }
+
+    public void loan(double amount) {
+        if (amount <= loanLimit) {
+            deposit(amount);
+        }
+    }
+}
