254f7ea1230a1b3f734a65191a2203a4e7e56f22
Leandro <lleandrofr@gmail.com>
2024-05-02 12:48:58 -0300

    feat: read file using FileReader and BufferedReader

--- 17-trabalhando-com-arquivos/read-file/src/application/Program.java
+++ 17-trabalhando-com-arquivos/read-file/src/application/Program.java
@@ -1,20 +1,53 @@
 package application;
 
-import java.io.File;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
 
-        File file = new File("./example-text.txt");
+//        File file = new File("./example-text.txt");
+//        try (Scanner sc = new Scanner(file)) {
+//            while (sc.hasNextLine()) {
+//                System.out.println(sc.nextLine());
+//            }
+//        } catch (IOException e) {
+//            System.out.println("Error: " + e.getMessage());
+//        }
 
-        try (Scanner sc = new Scanner(file)) {
-            while (sc.hasNextLine()) {
-                System.out.println(sc.nextLine());
+        String path = "./example-text.txt";
+        FileReader fr = null;
+        BufferedReader br = null;
+
+        try {
+            fr = new FileReader(path);
+            br = new BufferedReader(fr);
+
+            String line = br.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
             }
+
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
+        } finally {
+            try {
+                if (br != null) {
+                    br.close();
+                }
+                if (fr != null) {
+                    fr.close();
+                }
+
+            } catch (IOException e) {
+                System.out.println();
+            }
         }
+
     }
 }
