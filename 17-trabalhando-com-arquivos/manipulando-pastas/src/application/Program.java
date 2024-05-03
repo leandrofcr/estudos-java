98a147eac993c37f9d0854258388b5574eae4d74
Leandro <lleandrofr@gmail.com>
2024-05-03 12:05:28 -0300

    feat: get file name only

--- 17-trabalhando-com-arquivos/manipulando-pastas/src/application/Program.java
+++ 17-trabalhando-com-arquivos/manipulando-pastas/src/application/Program.java
@@ -15,14 +15,19 @@ public class Program {
         File[] folders = path.listFiles(File::isDirectory);
         System.out.println("FOLDERS:");
         for (File folder : folders) {
-            System.out.println(folder);
+            System.out.println("Path: " + folder);
+            System.out.println("Name: " + folder.getName());
+            System.out.println("---");
         }
 
         System.out.println();
         File[] files = path.listFiles(File::isFile);
         System.out.println("FILES:");
         for (File file : files) {
-            System.out.println(file);
+
+            System.out.println("Path: " + file);
+            System.out.println("Name: " + file.getName());
+            System.out.println("---");
         }
 
         boolean success = new File(strPath + "/subDir").mkdir();
