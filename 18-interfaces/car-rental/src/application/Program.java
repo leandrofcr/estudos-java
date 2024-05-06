55c7b1d4a295c46a8250c2d119fe88ba871ce745
Leandro <lleandrofr@gmail.com>
2024-05-06 20:48:44 -0300

    feat: implements main program logic

--- 18-interfaces/car-rental/src/application/Program.java
+++ 18-interfaces/car-rental/src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import model.entities.CarRental;
 import model.entities.Vehicle;
+import model.services.BrazilTaxService;
+import model.services.RentalService;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -25,14 +27,19 @@ public class Program {
         CarRental carRental = new CarRental(start, finish, new Vehicle(carModel));
 
         System.out.print("Enter price per hour: ");
+        double pricePerHour = sc.nextDouble();
         System.out.print("Enter price per day: ");
+        double pricePerDay = sc.nextDouble();
+
+        RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrazilTaxService());
+
+        rentalService.processInvoice(carRental);
 
         System.out.println();
         System.out.println("INVOICE");
-        System.out.println("Basic payment: ");
-        System.out.println("Tax: ");
-        System.out.println("Total payment: ");
-
+        System.out.println("Basic payment: "  + carRental.getInvoice().getBasicPayment() );
+        System.out.println("Tax: " + carRental.getInvoice().getTax());
+        System.out.println("Total payment: " + carRental.getInvoice().getTotalPayment());
 
         sc.close();
     }
